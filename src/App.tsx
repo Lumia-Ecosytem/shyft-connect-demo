de778913595655426a0bde1c249d1884dbd67e04
kiennguyen <djhahe@gmail.com>
2025-02-13 16:41:30 +0700

    update shyft url

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ const shyftWalelt :CustomWallet ={
   name: 'Shyft Wallet',
   homepage: 'https://shyft.tech',
   image_url: 'https://s3.us-east-1.amazonaws.com/asset.shyft.tech/shyft-logo-back-128.png',
-  webapp_link: 'https://dev.shyft-wallet-web.pages.dev/',
+  webapp_link: 'http://localhost:5173',
 }
 
 // Create modal
